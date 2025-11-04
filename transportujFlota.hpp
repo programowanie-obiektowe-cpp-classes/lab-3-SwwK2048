c341adccaa6f8c22a7bd0ff0fe29b2a4d25df313
Stanisław Walczak <osapo137@gmail.com>
2025-11-04 19:37:58 +0100

    commit1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
-#include "Stocznia.hpp"
+#include "include/Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int juz_przetransportowano = 0;
+    unsigned int zaglowce = 0;
+
+    if (towar == 0) return zaglowce;
+
+    Stocznia stocznia{};
+
+    while(juz_przetransportowano < towar)
+    {
+        Statek* s = stocznia();
+        juz_przetransportowano += s -> transportuj();
+
+        if (dynamic_cast<Zaglowiec*>(s)) zaglowce++;
+
+        delete s;
+    }
+
+    return zaglowce;
 }
